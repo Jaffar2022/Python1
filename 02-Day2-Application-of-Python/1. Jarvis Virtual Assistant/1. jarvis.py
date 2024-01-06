f68ed251039de889a2c2904238b0f9050dca1830
Jaffar2022 <jaffarm048@gmail.com>
2024-01-06 14:47:57 +0530

    changes

--- 02-Day2-Application-of-Python/1. Jarvis Virtual Assistant/1. jarvis.py
+++ 02-Day2-Application-of-Python/1. Jarvis Virtual Assistant/1. jarvis.py
@@ -30,7 +30,7 @@ def wishMe():
     else:
         speak("Good Evening!")  
 
-    speak("I am Jaffar Sir. Please tell me how may I help you")       
+    speak("I am Jarvis Sir. Please tell me how may I help you")       
 
 def takeCommand():
     #It takes microphone input from the user and returns string output
